d7410e4a20a871301a8c2f5cf9eaf6dd285b98c0
unknown <codeing999@gmail.com>
2023-08-27 03:54:28 +0900

    :memo: GameJam 풀이 완료

--- README.md
+++ README.md
@@ -11,4 +11,9 @@
 | 2023-08-05 | programmers level2 요격 시스템 풀기 (실패) |
 | 2023-08-06 | programmers level2 큰 수 만들기 풀기 (실패) |
 | 2023-08-20 | programmers level2 JadenCase 문자열 만들기 풀기 |
-|            |                                           |
+| 2023-08-27 | goorm challenge GameJam 풀기 |
+|  |  |
+|  |  |
+|  |  |
+|  |  |
+|  |  |
--- goorm/GameJam/GameJam.js
+++ goorm/GameJam/GameJam.js
@@ -15,15 +15,15 @@ rl.on('line', (line) => {
 	}
 	input = line;
 	if (i === 0){
-		N = input;
+		N = +input;
 	} else if (i === 1) {
 		let temp = input.split(' ');
-		gCoord.x = temp[0];
-		gCoord.y = temp[1];
+		gCoord.x = +temp[1] -1;
+		gCoord.y = +temp[0] -1;
 	} else if (i === 2) {
 		let temp = input.split(' ');
-		pCoord.x = temp[0];
-		pCoord.y = temp[1];
+		pCoord.x = +temp[1] -1;
+		pCoord.y = +temp[0] -1;
 	} else	if( (i > 2) && (i <= N+2) ){
 		board.push(input.split(' '));
 	}
@@ -31,8 +31,132 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', () => {
-	console.log(N);
-	console.log(gCoord);
-	console.log(pCoord);
-	console.log(board);
+	// console.log(N, typeof N);
+	// console.log(gCoord);
+	// console.log(pCoord);
+	// console.log(board);
+	let gPoint = 1;
+	let pPoint = 1;
+	// let gArray = Array.from(Array(N), () => Array(N).fill(0))
+	// let pArray = Array.from(Array(N), () => Array(N).fill(0))
+	// gArray[gCoord.y][gCoord.x] = 1;
+	// pArray[pCoord.y][pCoord.x] = 1;
+	let visitList = [];
+	visitList.push(`${gCoord.y},${gCoord.x}`)
+	
+	//goorm 플레이
+	let flag = true;
+	while(flag){
+		let count = +board[gCoord.y][gCoord.x].slice(0, -1);
+        let command = board[gCoord.y][gCoord.x].slice(-1);
+		let column = gCoord.x;
+		let row = gCoord.y;
+		//console.log(gCoord.x, gCoord.y, count, command)
+		for ( let i = 0; i < count; i++) {
+			if (command === 'U'){
+				row = row === 0? N-1 : row-1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					gPoint++;
+				}
+			} else if (command === 'D'){
+				row = row === N-1? 0 : row+1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					gPoint++;
+				}
+			} else if (command === 'R'){
+				column = column === N-1? 0 : column+1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					gPoint++;
+				}
+			} else if (command === 'L'){
+				column = column === 0? N-1 : column-1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					gPoint++;
+				}
+			}
+			if (i === count-1) {
+				gCoord.x = column;
+				gCoord.y = row;
+			}
+		}
+		
+	}
+	//console.log(gPoint)
+	
+	visitList = [];
+	visitList.push(`${pCoord.y},${pCoord.x}`)
+	
+	//player 플레이
+	flag = true;
+	while(flag){
+		let count = +board[pCoord.y][pCoord.x].slice(0, -1);
+        let command = board[pCoord.y][pCoord.x].slice(-1);
+		let column = pCoord.x;
+		let row = pCoord.y;
+		isDone = false;
+		//console.log(pCoord.x, pCoord.y, count, command)
+		for ( let i = 0; i < count; i++) {
+			if (command === 'U'){
+				row = row === 0? N-1 : row-1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					pPoint++;
+				}
+			} else if (command === 'D'){
+				row = row === N-1? 0 : row+1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					pPoint++;
+				}
+			} else if (command === 'R'){
+				column = column === N-1? 0 : column+1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					pPoint++;
+				}
+			} else if (command === 'L'){
+				column = column === 0? N-1 : column-1;
+				if ( visitList.includes(`${row},${column}`) ) {
+					flag = false;
+					break;
+				} else {
+					visitList.push(`${row},${column}`);
+					pPoint++;
+				}
+			}
+			if (i === count-1) {
+				pCoord.x = column;
+				pCoord.y = row;
+			}
+		}
+		
+	}
+	//console.log(pPoint)
+	console.log(gPoint > pPoint? `goorm ${gPoint}` : `player ${pPoint}`)
+	process.exit();
 })
